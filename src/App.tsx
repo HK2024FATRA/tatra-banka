2d1f94ea344c53e94bd981ce704b7465523f0732
Samuel Molcan <samuel.m.github@gmail.com>
2024-04-06 16:30:16 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import './App.css'
 
+import { Header } from './header/Header.tsx';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import wizard from './assets/wizard.png'
+
 function App() {
   return (
     <>
+      <Parallax pages={4}>
+
+          <ParallaxLayer
+              offset={0}
+              factor={0.5}>
+              <Header></Header>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+              offset={0.5}
+              factor={0.5}
+              speed={0.05}
+              style={{
+                  backgroundImage: `url(${wizard})`,
+                  height: '100%',
+              }}>
+          </ParallaxLayer>
+      </Parallax>
     </>
   )
 }
