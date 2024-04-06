be546eef83e5ed3a10a11c487e93cb85ea72e4b2
Samuel Molcan <samuel.m.github@gmail.com>
2024-04-06 19:50:41 +0200

    body

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 
 import { Header } from './header/Header.tsx';
 import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import img1 from './assets/img1.png';
 import wizard from './assets/wizard.png'
+import wizardFalling from './assets/wizard_falling.png';
 
 function App() {
   return (
@@ -26,17 +26,47 @@ function App() {
           <ParallaxLayer
               speed={0.1}
               offset={1}>
-              <div className="step1">
+              <div className="step">
                   <img src={wizard} alt={'wizzard'}/>
                   <h2>Ahoj! Volám sa FatraBot a budem tvojím pomocníkom na ceste investovania.</h2>
               </div>
           </ParallaxLayer>
 
-          <ParallaxLayer
-              speed={1}
-              offset={1}>
+
+          <ParallaxLayer offset={1.5} speed={0} sticky={{start: 2, end: 6}}>
+              <div style={{height: '100vh', display: 'flex', alignItems: 'flex-start'}}>
+                  <img src={wizardFalling} alt="Falling Wizard" style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'contain'}}/>
+              </div>
           </ParallaxLayer>
 
+          <ParallaxLayer speed={0.1} offset={3}>
+              <div className="step-falling">
+                  <div>
+                      <h2>Pokúsim sa ti jednoducho vysvetliť:</h2>
+                  </div>
+              </div>
+          </ParallaxLayer>
+          <ParallaxLayer speed={0.1} offset={4}>
+              <div className="step-falling">
+                  <div>
+                      <h2>Ako ti investovanie dokáže zhodnotiť úspory, ktoré ti ležia na účte 📈,</h2>
+                  </div>
+              </div>
+          </ParallaxLayer>
+          <ParallaxLayer speed={0.1} offset={5}>
+              <div className="step-falling">
+                  <div>
+                      <h2>vybudovať majetok a finančnú rezervu,</h2>
+                  </div>
+              </div>
+          </ParallaxLayer>
+          <ParallaxLayer speed={0.1} offset={6}>
+              <div className="step-falling">
+                  <div>
+                      <h2>a zabezpečiť tak tvoju finančnú stabilitu alebo slobodu.</h2>
+                  </div>
+              </div>
+          </ParallaxLayer>
 
 
           {/*<ParallaxLayer offset={1} speed={1} style={{*/}
